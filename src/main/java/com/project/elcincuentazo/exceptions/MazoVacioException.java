0835e3f1002265d9746dc108824dff3dceae7492
Leandro Castillo <elbert.moreno@correounivalle.edu.co>
2025-11-11 22:03:43 -0500

    Se agregó una excepción, la cual es una excepción propia marcada y se lanza cuando el mazo ya no tiene cartas y alguien intenta tomar una.

---
+++ src/main/java/com/project/elcincuentazo/exceptions/MazoVacioException.java
@@ -0,0 +1,7 @@
+package com.project.elcincuentazo.exceptions;
+
+public class MazoVacioException extends Exception {
+    public MazoVacioException(String message) {
+        super(message);
+    }
+}
